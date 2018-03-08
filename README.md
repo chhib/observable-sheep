1cf1efdfacfea70031336ada20ab7ec52282066b
David Jurelius <davan839@gmail.com>
2018-03-08 14:59:58 +0100

    Set up basic authentication with mirroring the secret key into a cookie

---
+++ README.md
@@ -0,0 +1,4 @@
+Small server used to retrieve Patreon pledges to visualize using Observable.
+
+Authentication is done through a cookie which has to match a secret key set in an environment variable.
+
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
+const app = express()
+
+const secretCookie = process.env.OBSERVABLE_AUTHENTICATION_COOKIE
+
+if (!secretCookie) {
+  console.log('Need secret set to environment variable OBSERVABLE_AUTHENTICATION_COOKIE.')
+  return;
+}
+
+app.use(cookieParser())
+app.get('/pledges', function (req, res) {
+  // Need authentication cookie
+  if (req.cookies['observable-authentication-cookie'] !== secretCookie) {
+    res.status(500).send('Need authentication.')
+    return; 
+  }
+  // Here you go
+  res.json(req.cookies)
+})
+app.get('/auth', function (req, res) {
+  if (!req.query['key']) {
+    res.status(500).send('Authenticate with /auth?key=THEKEY')
+    return;
+  }
+  res.cookie('observable-authentication-cookie', 
+    req.query['key'], { 
+      expires: new Date(Date.now() + 30*60*1000), 
+      httpOnly: true
+    }
+  )
+  res.status(200).send('Got it, thanks.')
+})
+
+const port = process.env.PORT || 8888
+app.listen(port, () => {
+  console.log(`Listening on port ${port}. Go /auth to set the cookie used for /pledges.`)
+})
